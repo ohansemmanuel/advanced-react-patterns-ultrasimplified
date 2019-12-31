a7ce83a8e61706f900532411214fb981504d116a
Ohans Emmanuel <ohansemmanuel@gmail.com>
2019-12-31 20:05:58 +0000

    handle initial scale animation

--- showcase/src/patterns/01.js
+++ showcase/src/patterns/01.js
@@ -13,9 +13,23 @@ const initialState = {
  */
 const withClapAnimation = WrappedComponent => {
   class WithClapAnimation extends Component {
+    animationTimeline = new mojs.Timeline()
     state = {
-      animationTimeline: new mojs.Timeline()
+      animationTimeline: this.animationTimeline
     }
+
+    componentDidMount () {
+      const scaleButton = new mojs.Html({
+        el: '#clap',
+        duration: 300,
+        scale: { 1.3: 1 },
+        easing: mojs.easing.out
+      })
+
+      const newAnimationTimeline = this.animationTimeline.add([scaleButton])
+      this.setState({ animationTimeline: newAnimationTimeline })
+    }
+
     render () {
       return (
         <WrappedComponent
@@ -46,7 +60,7 @@ const MediumClap = ({ animationTimeline }) => {
   }
 
   return (
-    <button className={styles.clap} onClick={handleClapClick}>
+    <button id='clap' className={styles.clap} onClick={handleClapClick}>
       <ClapIcon isClicked={isClicked} />
       <ClapCount count={count} />
       <CountTotal countTotal={countTotal} />
