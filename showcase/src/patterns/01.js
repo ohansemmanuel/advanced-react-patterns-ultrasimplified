b7856999c9163d7e16887bc777b8a7d833eae224
Ohans Emmanuel <ohansemmanuel@gmail.com>
2020-01-06 07:21:36 +0100

    handle countTotalAnimation

--- showcase/src/patterns/01.js
+++ showcase/src/patterns/01.js
@@ -19,17 +19,29 @@ const withClapAnimation = WrappedComponent => {
     }
 
     componentDidMount () {
+      const tlDuration = 300
       const scaleButton = new mojs.Html({
         el: '#clap',
-        duration: 300,
+        duration: tlDuration,
         scale: { 1.3: 1 },
         easing: mojs.easing.ease.out
+      })
+
+      const countTotalAnimation = new mojs.Html({
+        el: '#clapCountTotal',
+        opacity: { 0: 1 },
+        delay: (3 * tlDuration) / 2,
+        duration: tlDuration,
+        y: { 0: -3 }
       })
 
       const clap = document.getElementById('clap')
       clap.style.transform = 'scale(1,1)'
 
-      const newAnimationTimeline = this.animationTimeline.add([scaleButton])
+      const newAnimationTimeline = this.animationTimeline.add([
+        scaleButton,
+        countTotalAnimation
+      ])
       this.setState({ animationTimeline: newAnimationTimeline })
     }
 
@@ -94,7 +106,11 @@ const ClapCount = ({ count }) => {
 }
 
 const CountTotal = ({ countTotal }) => {
-  return <span className={styles.total}>{countTotal}</span>
+  return (
+    <span id='clapCountTotal' className={styles.total}>
+      {countTotal}
+    </span>
+  )
 }
 
 /**
