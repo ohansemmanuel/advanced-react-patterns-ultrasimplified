f0990d557335e9798661c3a107809468a05aef3d
Ohans Emmanuel <ohansemmanuel@gmail.com>
2020-01-07 06:39:45 +0100

    fix: remove misleading comment

--- showcase/src/patterns/index.js
+++ showcase/src/patterns/index.js
@@ -29,96 +29,93 @@ const useClapAnimation = ({
     new mojs.Timeline()
   )
 
-  useLayoutEffect(
-    () => {
-      if (!bounceEl || !fadeEl || !burstEl) {
-        return
-      }
+  useLayoutEffect(() => {
+    if (!bounceEl || !fadeEl || !burstEl) {
+      return
+    }
 
-      const triangleBurst = new mojs.Burst({
-        parent: burstEl,
-        radius: { 50: 95 },
-        count: 5,
-        angle: 30,
-        children: {
-          shape: 'polygon',
-          radius: { 6: 0 },
-          scale: 1,
-          stroke: 'rgba(211,84,0 ,0.5)',
-          strokeWidth: 2,
-          angle: 210,
-          delay: 30,
-          speed: 0.2,
-          easing: mojs.easing.bezier(0.1, 1, 0.3, 1),
-          duration: tlDuration
-        }
-      })
-
-      const circleBurst = new mojs.Burst({
-        parent: burstEl,
-        radius: { 50: 75 },
-        angle: 25,
-        duration: tlDuration,
-        children: {
-          shape: 'circle',
-          fill: 'rgba(149,165,166 ,0.5)',
-          delay: 30,
-          speed: 0.2,
-          radius: { 3: 0 },
-          easing: mojs.easing.bezier(0.1, 1, 0.3, 1)
-        }
-      })
-
-      const countAnimation = new mojs.Html({
-        el: bounceEl,
-        isShowStart: false,
-        isShowEnd: true,
-        y: { 0: -30 },
-        opacity: { 0: 1 },
+    const triangleBurst = new mojs.Burst({
+      parent: burstEl,
+      radius: { 50: 95 },
+      count: 5,
+      angle: 30,
+      children: {
+        shape: 'polygon',
+        radius: { 6: 0 },
+        scale: 1,
+        stroke: 'rgba(211,84,0 ,0.5)',
+        strokeWidth: 2,
+        angle: 210,
+        delay: 30,
+        speed: 0.2,
+        easing: mojs.easing.bezier(0.1, 1, 0.3, 1),
         duration: tlDuration
-      }).then({
-        opacity: { 1: 0 },
-        y: -80,
-        delay: tlDuration / 2
-      })
-
-      const countTotalAnimation = new mojs.Html({
-        el: fadeEl,
-        isShowStart: false,
-        isShowEnd: true,
-        opacity: { 0: 1 },
-        delay: (3 * tlDuration) / 2,
-        duration: tlDuration,
-        y: { 0: -3 }
-      })
-
-      const scaleButton = new mojs.Html({
-        el: burstEl,
-        duration: tlDuration,
-        scale: { 1.3: 1 },
-        easing: mojs.easing.out
-      })
-
-      if (typeof burstEl === 'string') {
-        clap.style.transform = 'scale(1, 1)'
-        const el = document.getElementById(id)
-        el.style.transform = 'scale(1, 1)'
-      } else {
-        burstEl.style.transform = 'scale(1, 1)'
       }
+    })
 
-      const updatedAnimationTimeline = animationTimeline.add([
-        countAnimation,
-        countTotalAnimation,
-        scaleButton,
-        circleBurst,
-        triangleBurst
-      ])
+    const circleBurst = new mojs.Burst({
+      parent: burstEl,
+      radius: { 50: 75 },
+      angle: 25,
+      duration: tlDuration,
+      children: {
+        shape: 'circle',
+        fill: 'rgba(149,165,166 ,0.5)',
+        delay: 30,
+        speed: 0.2,
+        radius: { 3: 0 },
+        easing: mojs.easing.bezier(0.1, 1, 0.3, 1)
+      }
+    })
 
-      setAnimationTimeline(updatedAnimationTimeline)
-    },
-    [tlDuration, animationTimeline, bounceEl, fadeEl, burstEl]
-  )
+    const countAnimation = new mojs.Html({
+      el: bounceEl,
+      isShowStart: false,
+      isShowEnd: true,
+      y: { 0: -30 },
+      opacity: { 0: 1 },
+      duration: tlDuration
+    }).then({
+      opacity: { 1: 0 },
+      y: -80,
+      delay: tlDuration / 2
+    })
+
+    const countTotalAnimation = new mojs.Html({
+      el: fadeEl,
+      isShowStart: false,
+      isShowEnd: true,
+      opacity: { 0: 1 },
+      delay: (3 * tlDuration) / 2,
+      duration: tlDuration,
+      y: { 0: -3 }
+    })
+
+    const scaleButton = new mojs.Html({
+      el: burstEl,
+      duration: tlDuration,
+      scale: { 1.3: 1 },
+      easing: mojs.easing.out
+    })
+
+    if (typeof burstEl === 'string') {
+      clap.style.transform = 'scale(1, 1)'
+      const el = document.getElementById(id)
+      el.style.transform = 'scale(1, 1)'
+    } else {
+      burstEl.style.transform = 'scale(1, 1)'
+    }
+
+    const updatedAnimationTimeline = animationTimeline.add([
+      countAnimation,
+      countTotalAnimation,
+      scaleButton,
+      circleBurst,
+      triangleBurst
+    ])
+
+    setAnimationTimeline(updatedAnimationTimeline)
+  }, [tlDuration, animationTimeline, bounceEl, fadeEl, burstEl])
 
   return animationTimeline
 }
@@ -158,7 +155,6 @@ const MediumClap = ({ children, onClap }) => {
   })
 
   const handleClapClick = () => {
-    // 👉 prop from HOC
     animationTimeline.replay()
 
     setClapState({
@@ -170,15 +166,12 @@ const MediumClap = ({ children, onClap }) => {
 
   const componentJustMounted = useRef(true)
 
-  useEffect(
-    () => {
-      if (!componentJustMounted.current) {
-        onClap(clapState)
-      }
-      componentJustMounted.current = false
-    },
-    [count, onClap]
-  )
+  useEffect(() => {
+    if (!componentJustMounted.current) {
+      onClap(clapState)
+    }
+    componentJustMounted.current = false
+  }, [count, onClap])
 
   const memoizedValue = useMemo(
     () => ({
