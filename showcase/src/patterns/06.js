708aec5085e539bf1a5af4c1ef0f2bcada75c2ad
Ohans Emmanuel <ohansemmanuel@gmail.com>
2020-01-24 06:06:11 +0100

    fix hook: useClapState

--- showcase/src/patterns/06.js
+++ showcase/src/patterns/06.js
@@ -2,7 +2,7 @@ import React, { useState, useLayoutEffect, useCallback } from 'react'
 import mojs from 'mo-js'
 import styles from './index.css'
 
-const initialState = {
+const INITIAL_STATE = {
   count: 0,
   countTotal: 267,
   isClicked: false
@@ -117,9 +117,27 @@ const useDOMRef = () => {
   return [DOMRef, setRef]
 }
 
-const MediumClap = () => {
+/**
+ * custom hook for useClapState
+ */
+const useClapState = (initialState = INITIAL_STATE) => {
   const MAXIMUM_USER_CLAP = 50
   const [clapState, setClapState] = useState(initialState)
+  const { count, countTotal } = clapState
+
+  const updateClapState = useCallback(() => {
+    setClapState(({ count, countTotal }) => ({
+      isClicked: true,
+      count: Math.min(count + 1, MAXIMUM_USER_CLAP),
+      countTotal: count < MAXIMUM_USER_CLAP ? countTotal + 1 : countTotal
+    }))
+  }, [count, countTotal])
+
+  return [clapState, updateClapState]
+}
+
+const MediumClap = () => {
+  const [clapState, updateClapState] = useClapState()
   const { count, countTotal, isClicked } = clapState
 
   const [{ clapRef, clapCountRef, clapTotalRef }, setRef] = useDOMRef()
@@ -132,14 +150,7 @@ const MediumClap = () => {
 
   const handleClapClick = () => {
     animationTimeline.replay()
-    setClapState(prevState => ({
-      isClicked: true,
-      count: Math.min(count + 1, MAXIMUM_USER_CLAP),
-      countTotal:
-        count < MAXIMUM_USER_CLAP
-          ? prevState.countTotal + 1
-          : prevState.countTotal
-    }))
+    updateClapState()
   }
 
   return (
